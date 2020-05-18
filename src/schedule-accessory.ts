5a14851137790edfd1704c2774c530a50e77f5a8
Kevin Brashears <47545863+kbrashears5@users.noreply.github.com>
2020-05-17 20:55:01 -0500

    logic for turning off immediately after turning on (#1)

--- src/schedule-accessory.ts
+++ src/schedule-accessory.ts
@@ -2,6 +2,7 @@ import {
 	AccessoryConfig,
 	AccessoryPlugin,
 	API,
+	Characteristic,
 	CharacteristicEventTypes,
 	CharacteristicGetCallback,
 	CharacteristicSetCallback,
@@ -18,7 +19,7 @@ let hap: HAP;
  */
 export = (api: API) => {
 	hap = api.hap;
-	api.registerAccessory('ScheduleAccessory', ScheduleAccessory);
+	api.registerAccessory('Schedule', ScheduleAccessory);
 };
 
 class ScheduleAccessory implements AccessoryPlugin {
@@ -34,21 +35,34 @@ class ScheduleAccessory implements AccessoryPlugin {
 		this.log = log;
 		this.name = config.name;
 
+		log.info(`Name: ${config.name}`)
+		log.info(`Interval: ${config.interval}`)
+
 		this.switchService = new hap.Service.Switch(this.name);
 
 		this.switchService.getCharacteristic(hap.Characteristic.On)
 			.on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
 				this.log.info('Schedule: ' + (this.scheduleOn ? 'ON' : 'OFF'));
+
 				callback(undefined, this.scheduleOn);
 			})
 			.on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
 				this.scheduleOn = value as boolean;
+
 				this.log.info('Schedule was set to: ' + (this.scheduleOn ? 'ON' : 'OFF'));
+
+				if (value) {
+					setTimeout(() => {
+						this.switchService.setCharacteristic(Characteristic.On, false);
+					}, 1000);
+				}
+
 				callback();
 			});
 
 		this.informationService = new hap.Service.AccessoryInformation()
 			.setCharacteristic(hap.Characteristic.Manufacturer, 'Homebridge Schedule')
+			.setCharacteristic(hap.Characteristic.SerialNumber, '123456789')
 			.setCharacteristic(hap.Characteristic.Model, config.interval);
 
 		log.info('Initialization complete');
