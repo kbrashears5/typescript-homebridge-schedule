6334084cc037968bd0f98e48de73281c6727ffdd
Kevin Brashears <47545863+kbrashears5@users.noreply.github.com>
2021-02-26 22:32:03 -0600

    converting to eslint (#73)
    * converting to eslint
    
    * updating packages

--- README.md
+++ README.md
@@ -12,20 +12,24 @@
 
 </div>
 
-This [Homebridge](https://github.com/nfarina/homebridge) plugin allows you to create Homekit automations for more than just the default "A time of day occurrs"
+This [Homebridge](https://github.com/nfarina/homebridge) plugin allows you to create HomeKit automations for more than just the default "A time of day occurs"
 
 The original thought for this was to create an hourly automation to check my thermostat in the house and send me a notification if it was above or below a certain threshold
 
-Homebridge-Schedule is similar to [Homebridge-Dummy](https://github.com/nfarina/homebridge-dummy), in the sense that it creates some dummy switches in Homekit that turn on at your desired interval. They will turn off one second later. This is useful for triggering automations, or even shortcuts.
+Homebridge-Schedule is similar to [Homebridge-Dummy](https://github.com/nfarina/homebridge-dummy), in the sense that it creates some dummy switches in HomeKit that turn on at your desired interval. They will turn off one second later. This is useful for triggering automations, or even shortcuts.
 
 ## Installation
+
 ```
 sudo npm i homebridge-schedule@latest -g
 ```
 
 ## Usage
+
 ### Interval Based
+
 Add accessories to your `config.json` similar to below for interval based schedules:
+
 ```json
 {
   "accessories": [
@@ -37,19 +41,23 @@ Add accessories to your `config.json` similar to below for interval based schedu
   ]
 }
 ```
-| Property | Description |
-| --- | --- |
-| Accessory | Must be "Schedule" |
-| Name | Unique name for the dummy switch |
-| Interval | Interval, in minutes |
+
+| Property  | Description                      |
+| --------- | -------------------------------- |
+| Accessory | Must be "Schedule"               |
+| Name      | Unique name for the dummy switch |
+| Interval  | Interval, in minutes             |
 
 Upon startup of Homebridge, the device will turn on at the specified interval
 
 #### Notes
+
 The interval starts when Homebridge is started up. If you want something to run hourly on the hour, then you need to make sure Homebridge is started up on the hour
 
 ### Cron Based
+
 Add accessories to your `config.json` similar to below for cron based schedules:
+
 ```json
 {
   "accessories": [
@@ -61,23 +69,27 @@ Add accessories to your `config.json` similar to below for cron based schedules:
   ]
 }
 ```
-| Property | Description |
-| --- | --- |
-| Accessory | Must be "Schedule" |
-| Name | Unique name for the dummy switch |
-| Cron | Cron string |
+
+| Property  | Description                      |
+| --------- | -------------------------------- |
+| Accessory | Must be "Schedule"               |
+| Name      | Unique name for the dummy switch |
+| Cron      | Cron string                      |
 
 Cron string details: https://www.npmjs.com/package/cron
 
 Cron string uses seconds, so for an hourly cron string use:
-> "0 0 * * * *"
+
+> "0 0 \* \* \* \*"
 
 > "{seconds} {minutes} {hours} {days} {months} {weeks}"
 
 ## Use Cases
-I have created a "room" called Automation in my Homekit, which then allows me to create an automation "When the Automation Hourly turns on" to check my thrermostat temperature, check the current outside temperature at my house, and based on some conditions, set the thermostat.
+
+I have created a "room" called Automation in my HomeKit, which then allows me to create an automation "When the Automation Hourly turns on" to check my thermostat temperature, check the current outside temperature at my house, and based on some conditions, set the thermostat.
 
 Steps:
+
 1. Create new automation based on "An Accessory is Controlled"
 2. Find your newly created dummy switch
 3. Select Turns On and add any desired time or people conditions
@@ -85,6 +97,7 @@ Steps:
 5. Done! You now have one automation for things you want to happen hourly rather than 24
 
 ## Creating HomeKit Automations
+
 Create new Accessory automation:
 ![Step1](https://github.com/kbrashears5/homebridge-schedule/blob/master/images/step1.jpg?raw=true)
 
@@ -98,13 +111,14 @@ Scroll all the way to the bottom and choose `Convert to Shortcut`
 ![Step4](https://github.com/kbrashears5/homebridge-schedule/blob/master/images/step4.jpg?raw=true)
 
 Create shortcut:
-1. Get the current temperature of a Homekit thermostat
+
+1. Get the current temperature of a HomeKit thermostat
 2. Convert to Fahrenheit
 3. If the indoor temperature is greater than a threshold
    - Get current weather outside
    - Convert to Fahrenheit
    - If the current weather outside is also above a threshold
-      - Turn on fans, set thermostat, etc.
+     - Turn on fans, set thermostat, etc.
 
 ![Step5](https://github.com/kbrashears5/homebridge-schedule/blob/master/images/step5_1.jpg?raw=true)
 ![Step5](https://github.com/kbrashears5/homebridge-schedule/blob/master/images/step5_2.jpg?raw=true)
--- src/schedule-accessory.ts
+++ src/schedule-accessory.ts
@@ -1,14 +1,14 @@
 import {
-	AccessoryConfig,
-	AccessoryPlugin,
-	API,
-	CharacteristicEventTypes,
-	CharacteristicGetCallback,
-	CharacteristicSetCallback,
-	CharacteristicValue,
-	HAP,
-	Logging,
-	Service,
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  HAP,
+  Logging,
+  Service,
 } from 'homebridge';
 import { ObjectOperations } from 'typescript-helper-functions';
 import { CronJob } from 'cron';
@@ -19,115 +19,118 @@ let hap: HAP;
  * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-	hap = api.hap;
-	api.registerAccessory('homebridge-schedule', 'Schedule', ScheduleAccessory);
+  hap = api.hap;
+  api.registerAccessory('homebridge-schedule', 'Schedule', ScheduleAccessory);
 };
 
 class ScheduleAccessory implements AccessoryPlugin {
-	private readonly log: Logging;
-	private readonly name: string;
-	private scheduleOn = false;
+  private readonly log: Logging;
+  private readonly name: string;
+  private scheduleOn = false;
 
-	private readonly switchService: Service;
-	private readonly informationService: Service;
+  private readonly switchService: Service;
+  private readonly informationService: Service;
 
-	private readonly objectOperations: ObjectOperations;
+  private readonly objectOperations: ObjectOperations;
 
-	constructor(log: Logging,
-		config: AccessoryConfig) {
+  constructor(log: Logging, config: AccessoryConfig) {
+    this.objectOperations = new ObjectOperations();
 
-		this.objectOperations = new ObjectOperations();
+    this.log = log;
+    this.name = config.name;
 
-		this.log = log;
-		this.name = config.name;
+    // log config parameters
+    log.debug(`Name: [${config.name}]`);
+    log.debug(`Interval: [${config.interval}]`);
+    log.debug(`Cron: [${config.cron}]`);
 
-		// log config parameters
-		log.debug(`Name: [${config.name}]`)
-		log.debug(`Interval: [${config.interval}]`)
-		log.debug(`Cron: [${config.cron}]`)
+    // determine what was provided by config
+    let intervalSupplied = true;
+    if (config.interval === undefined) {
+      intervalSupplied = false;
+    }
+    log.debug(`Interval param supplied: [${intervalSupplied}]`);
 
-		// determine what was provided by config
-		let intervalSupplied = true;
-		if (config.interval === undefined) {
-			intervalSupplied = false;
-		}
-		log.debug(`Interval param supplied: [${intervalSupplied}]`);
+    const cronSupplied = !this.objectOperations.IsNullOrWhitespace(config.cron);
+    log.debug(`Cron param supplied: [${cronSupplied}]`);
 
-		const cronSupplied = !this.objectOperations.IsNullOrWhitespace(config.cron);
-		log.debug(`Cron param supplied: [${cronSupplied}]`);
+    // if neither params were supplied
+    if (!intervalSupplied && !cronSupplied) {
+      log.error('Must supply either interval or cron');
+    }
+    // if both supplied
+    else if (intervalSupplied && cronSupplied) {
+      log.error('Cannot have both interval and cron. Choose one or the other');
+    }
 
-		// if neither params were supplied
-		if (!intervalSupplied && !cronSupplied) {
-			log.error('Must supply either interval or cron');
-		}
-		// if both supplied
-		else if (intervalSupplied && cronSupplied) {
-			log.error('Cannot have both interval and cron. Choose one or the other');
-		}
+    // create accessory
+    log.info(`Creating schedule accessory [${config.name}]`);
 
-		// create accessory
-		log.info(`Creating schedule accessory [${config.name}]`);
+    this.switchService = new hap.Service.Switch(this.name);
 
-		this.switchService = new hap.Service.Switch(this.name);
+    this.switchService
+      .getCharacteristic(hap.Characteristic.On)
+      .on(
+        CharacteristicEventTypes.GET,
+        (callback: CharacteristicGetCallback) => {
+          this.log.info(`Schedule: [${this.scheduleOn ? 'ON' : 'OFF'}]`);
 
-		this.switchService.getCharacteristic(hap.Characteristic.On)
-			.on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-				this.log.info(`Schedule: [${this.scheduleOn ? 'ON' : 'OFF'}]`);
+          callback(undefined, this.scheduleOn);
+        },
+      )
+      .on(
+        CharacteristicEventTypes.SET,
+        (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+          this.scheduleOn = value as boolean;
 
-				callback(undefined, this.scheduleOn);
-			})
-			.on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-				this.scheduleOn = value as boolean;
+          this.log.info(
+            `Schedule was set to: [${this.scheduleOn ? 'ON' : 'OFF'}]`,
+          );
 
-				this.log.info(`Schedule was set to: [${this.scheduleOn ? 'ON' : 'OFF'}]`);
+          if (value) {
+            setTimeout(() => {
+              this.switchService.setCharacteristic('On', false);
+            }, 1000);
+          }
 
-				if (value) {
-					setTimeout(() => {
-						this.switchService.setCharacteristic('On', false);
-					}, 1000);
-				}
+          callback();
+        },
+      );
 
-				callback();
-			});
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, 'Homebridge Schedule')
+      .setCharacteristic(hap.Characteristic.SerialNumber, '123456789')
+      .setCharacteristic(hap.Characteristic.Model, config.interval);
 
-		this.informationService = new hap.Service.AccessoryInformation()
-			.setCharacteristic(hap.Characteristic.Manufacturer, 'Homebridge Schedule')
-			.setCharacteristic(hap.Characteristic.SerialNumber, '123456789')
-			.setCharacteristic(hap.Characteristic.Model, config.interval);
+    log.info('Initialization complete');
 
-		log.info('Initialization complete');
+    // interval
+    if (intervalSupplied && !cronSupplied) {
+      log.info(`Starting [${config.interval}] minute interval`);
 
-		// interval
-		if (intervalSupplied && !cronSupplied) {
-			log.info(`Sarting [${config.interval}] minute interval`);
+      setInterval(() => {
+        this.switchService.setCharacteristic('On', true);
+      }, config.interval * 60000);
+    }
+    // cron
+    else if (!intervalSupplied && cronSupplied) {
+      log.info(`Starting [${config.cron}] cron job`);
 
-			setInterval(() => {
-				this.switchService.setCharacteristic('On', true);
-			}, (config.interval * 60000));
-		}
-		// cron
-		else if (!intervalSupplied && cronSupplied) {
-			log.info(`Sarting [${config.cron}] cron job`);
+      const job = new CronJob(config.cron, () => {
+        this.switchService.setCharacteristic('On', true);
+      });
 
-			const job = new CronJob(config.cron, () => {
-				this.switchService.setCharacteristic('On', true);
-			});
+      job.start();
+    } else {
+      log.error('Nothing started - check config');
+    }
+  }
 
-			job.start();
-		}
-		else {
-			log.error('Nothing started - check config');
-		}
-	}
-
-	/*
-	 * This method is called directly after creation of this instance.
-	 * It should return all services which should be added to the accessory.
-	 */
-	getServices(): Service[] {
-		return [
-			this.informationService,
-			this.switchService,
-		];
-	}
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [this.informationService, this.switchService];
+  }
 }
